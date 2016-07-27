a43b18a34b4f0887bbd3a45a2db823004e047d43
Glenn Spinner <glenngineer1@gmail.com>
2016-07-27 17:45:34 -0500

    posting movies to browser

---
+++ app/app.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var app = angular.module("MovieApp", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+  $routeProvider.
+   when('/login', {
+     templateUrl: 'partials/loginRegister.html',
+     controller: 'loginRegisterCtrl'
+   })
+   .when('/searchDatabase', {
+     templateUrl: 'partials/searchDatabase.html',
+     controller: 'searchDatabaseCtrl'
+   });
+});
